554444060b397d89d69f3a68cab3b84aa368bdc7
Rob Brown <rabrown.mail@gmail.com>
2013-03-17 02:04:32 +0000

    Removed old rails folder

--- RoR/app/models/comment.rb
+++
@@ -1,4 +0,0 @@
-class Comment < ActiveRecord::Base
-  attr_accessible :article_id, :body, :email, :name
-  belongs_to :article
-end
--- RoR/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-RoR::Application.config.secret_token = '8f29f70ad236dc74693704b43a2e123fb67d9b5d9420f2ad38557893379de9b9f0105b2b99d3809be39706ed46d123f54adb9fc944cc64117ce4869744ada8a1'
