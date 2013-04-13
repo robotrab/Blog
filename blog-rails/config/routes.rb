628e4dd85c4b3cebce74c22f1492d0aec1d4456c
Rob Brown <rabrown.mail@gmail.com>
2013-04-13 19:06:25 -0400

    Added password authentication.

--- blog-rails/config/routes.rb
+++ blog-rails/config/routes.rb
@@ -1,6 +1,9 @@
 BlogRails::Application.routes.draw do
+  get "log_in" => "sessions#new", :as => "log_in"
+  get "log_out" => "sessions#destroy", :as => "log_out"
   get "sign_up" => "users#new", :as => "sign_up"
   resources :users
+  resources :sessions
 
   resources :posts do
     resources :comments
